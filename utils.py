7147fc34b2149b22dc75c54bf35c9d9f4effd156
Midkey <huang6469@qq.com>
2023-04-11 00:01:56 +0800

    Add chats glm support.

--- utils.py
+++ utils.py
@@ -12,5 +12,7 @@ def get_chat_interface(model_type, batch_enabled):
         return flan.chat_batch if batch_enabled else flan.chat_stream
     elif model_type == "llama":
         return llama_rlhf.chat_batch if batch_enabled else llama_rlhf.chat_stream    
+    elif model_type == "glm":
+        return glm.chat_batch if batch_enabled else glm.chat_stream    
     else:
         return None
